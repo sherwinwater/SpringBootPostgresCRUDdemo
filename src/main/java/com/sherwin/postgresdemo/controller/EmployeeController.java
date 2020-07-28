5171be18477460f4d3e4ee39523126c027af783c
sherwin <sherwin.water@gmail.com>
2020-07-28 18:10:38 -0400

    postgres db demo for heroku

--- src/main/java/com/sherwin/postgresdemo/controller/EmployeeController.java
+++ src/main/java/com/sherwin/postgresdemo/controller/EmployeeController.java
@@ -1,26 +1,13 @@
 package com.sherwin.postgresdemo.controller;
 
-import com.sherwin.postgresdemo.model.Employee;
-import com.sherwin.postgresdemo.repository.EmployeeRepository;
-import com.sherwin.postgresdemo.service.EmployeeService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
-@RestController
-@RequestMapping("/api")
+@Controller
 public class EmployeeController {
 
-	@Autowired
-	private EmployeeService employeeService;
-
-	@GetMapping("/employees")
-	public List<Employee> getAllEmployees() {
-
-		return employeeService.findAll();
-	}
-
+    @GetMapping("/")
+    public String home(){
+        return "index";
+    }
 }
