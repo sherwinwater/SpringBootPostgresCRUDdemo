aaa56dbd3430148768ba32942566b95099da78d9
sherwin <sherwin.water@gmail.com>
2020-07-29 18:37:33 -0400

    add mailgun send email

--- src/main/java/com/sherwin/postgresdemo/controller/EmployeeController.java
+++ src/main/java/com/sherwin/postgresdemo/controller/EmployeeController.java
@@ -1,13 +1,24 @@
 package com.sherwin.postgresdemo.controller;
 
+import com.sherwin.postgresdemo.model.Employee;
+import com.sherwin.postgresdemo.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class EmployeeController {
 
+    @Autowired
+    private EmployeeService employeeService;
+
     @GetMapping("/")
-    public String home(){
+    public String home(Model model){
+        List<Employee> employees = employeeService.findAll();
+        model.addAttribute("employees",employees);
         return "index";
     }
 }
