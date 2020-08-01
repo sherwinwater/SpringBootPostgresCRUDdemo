3e75771ab821812fcea3010e0af51acfa666847c
sherwin <sherwin.water@gmail.com>
2020-07-31 22:56:37 -0400

    Change layout

--- src/main/java/com/sherwin/postgresdemo/home/HomeController.java
+++ src/main/java/com/sherwin/postgresdemo/home/HomeController.java
@@ -26,6 +26,16 @@ public class HomeController {
         return "layout";
     }
 
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/signup")
+    public String logout() {
+        return "signup";
+    }
+
 
 //    @GetMapping("/main")
 //    public String mainPage() {
